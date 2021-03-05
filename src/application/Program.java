01b2677615d63b2fac44a2393503a4deebb2e4ca
Rian Sousa <orionplay9@gmail.com>
2021-03-05 11:35:52 -0300

    Adicionado discordancia

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,16 @@ package application;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		System.out.println("Salve Salve Familia");
 		System.out.println("aaaa discordo");
 		System.out.println("Caraaalho");
 		
+		for(int i=0;i<10;i++) {
+			int count = i;
+			System.out.println("Discordancia nº" + count);
+		}
+		
 		
 	}
-
 }
