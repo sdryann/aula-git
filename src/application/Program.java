b6e1c6ad3bce927b3b64e2bc62d607a7d3e2426a
Rian Sousa <orionplay9@gmail.com>
2021-03-05 11:33:38 -0300

    Adicionado discordo e caraaalho

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,12 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Salve Salve Familia");
-
+		System.out.println("aaaa discordo");
+		System.out.println("Caraaalho");
+		
+		
 	}
 
 }
